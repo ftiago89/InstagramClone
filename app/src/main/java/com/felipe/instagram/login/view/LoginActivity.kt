75d1c70f6c5f0528a3915d13cfe4e05fb2e1797d
FELIPE-PESSOAL\ftiag <ftiagomelo89@gmail.com>
2022-10-05 08:03:38 -0300

    - Adicionando listeners para editTexts de email e senha;

--- app/src/main/java/com/felipe/instagram/login/view/LoginActivity.kt
+++ app/src/main/java/com/felipe/instagram/login/view/LoginActivity.kt
@@ -1,12 +1,36 @@
 package com.felipe.instagram.login.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.felipe.instagram.R
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val editTextEmail = findViewById<TextInputEditText>(R.id.login_edit_email)
+        val editTextPassword = findViewById<TextInputEditText>(R.id.login_edit_password)
+
+        editTextEmail.addTextChangedListener(watcher)
+        editTextPassword.addTextChangedListener(watcher)
+    }
+
+    private val watcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            findViewById<Button>(R.id.login_btn_enter).isEnabled = s.toString().isNotEmpty()
+        }
+
+        override fun afterTextChanged(p0: Editable?) {
+        }
+
     }
 }
+
