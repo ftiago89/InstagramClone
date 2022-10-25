66db845ed6e61cf76532840b48a1e947bff8ce64
FELIPE-PESSOAL\ftiag <ftiagomelo89@gmail.com>
2022-10-25 07:29:48 -0300

    - Iniciando activity de registro de conta; - Adicionando layout de registro de email;

---
+++ app/src/main/java/com/felipe/instagram/register/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.felipe.instagram.register.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.felipe.instagram.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
