4a07967694a6a93081762ce5d6571aa54ef3dbf1
FELIPE-PESSOAL\ftiag <ftiagomelo89@gmail.com>
2023-07-17 08:03:03 -0300

    - Adicionando main activity e iniciando fragment de perfil de usuário.

---
+++ app/src/main/java/com/felipe/instagram/main/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.felipe.instagram.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.felipe.instagram.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
